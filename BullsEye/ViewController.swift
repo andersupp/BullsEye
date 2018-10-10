5e75863db9082c8c1d86117e1bcfc2ff8f636116
Anders Uppsäll <anders.uppsall@gmail.com>
2018-10-10 19:04:39 +0200

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Anders Uppsäll on 2018-10-10.
+//  Copyright © 2018 Anders Uppsäll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showAlert() {
+        
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+
+
+}
+
