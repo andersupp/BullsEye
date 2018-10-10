1bd4a7a5c8cb37109f66dafc0b20b1c5fda9dd47
Anders U <anders.uppsall@gmail.com>
2018-10-10 21:40:37 +0200

    Just starting part 3 adding polish

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
     
     @IBAction func showAlert() {
         
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
@@ -25,8 +44,31 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
     }
-
-
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round = round + 1
+        updateLabels()
+        
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
 }
 
