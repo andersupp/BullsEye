aecc578c41ad59901ef5000bb9c66ab2492f15a7
Anders U <anders.uppsall@gmail.com>
2018-10-11 21:31:26 +0200

    lite mer

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Anders Uppsäll on 2018-10-11.
+//  Copyright © 2018 Anders Uppsäll. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Anders Uppsäll on 2018-10-11.
+//  Copyright © 2018 Anders Uppsäll. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
